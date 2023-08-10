207fe4d1dd2198751594c8960f8ac2798a9115eb
nakamori akira <nakamori.1224@gmail.com>
2023-08-10 10:36:30 +0900

    🔥 不要につき削除（初コミット）

--- pages/beans/[id].js
+++ pages/beans/[id].js
@@ -1,7 +1,5 @@
 import Head from "next/head";
 import { Header } from "@/components/Header";
-import { useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import styles from "@/styles/Home.module.css";
 
