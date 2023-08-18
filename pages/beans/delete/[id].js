e80e975d51ea96a6be265280ab2048905a14e217
akiran1sei <nakamori.1224@gmail.com>
2023-08-18 10:47:09 +0900

    🎨 VercelURLに変更

--- pages/beans/delete/[id].js
+++ pages/beans/delete/[id].js
@@ -347,8 +347,8 @@ export default DeleteItem;
 
 export const getServerSideProps = async (context) => {
   const response = await fetch(
-    `http://localhost:3000/api/beans/${context.query.id}`
-    // `https://tasting-note.vercel.app/api/beans/${context.query.id}`
+    // `http://localhost:3000/api/beans/${context.query.id}`
+    `https://tasting-note.vercel.app/api/beans/${context.query.id}`
   );
   const singleItem = await response.json();
 
