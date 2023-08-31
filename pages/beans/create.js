6e5577eb40d5aad4f691144d58105e3c41024792
akiran1sei <nakamori.1224@gmail.com>
2023-08-31 12:31:42 +0900

    🎨 編集項目の説明文付与とスタイル調整

--- pages/beans/create.js
+++ pages/beans/create.js
@@ -201,14 +201,19 @@ const New = () => {
                   <li className={styles.edit_number_item}>作成者名</li>
                   <li className={styles.edit_number_item}>
                     1:コーヒー豆の名前 or 番号
-                    <span>豆の名前、又は、番号</span>
+                    <br />
+                    <span className={styles.edit_number_sup}>
+                      豆の名前、又は、番号
+                    </span>
                   </li>
                   <li className={styles.edit_number_item}>
-                    2:ロースト <span>焙煎具合</span>
+                    2:ロースト <br />
+                    <span className={styles.edit_number_sup}>焙煎具合</span>
                   </li>
                   <li className={styles.edit_number_item}>
                     3:アロマ
-                    <span>
+                    <br />
+                    <span className={styles.edit_number_sup}>
                       粉の状態の『ドライ』
                       <br />
                       湯を注いだ直後の『クラスト』
@@ -220,46 +225,65 @@ const New = () => {
                   </li>
                   <li className={styles.edit_number_item}>
                     4:欠点・瑕疵
-                    <span>
+                    <br />
+                    <span className={styles.edit_number_sup}>
                       スペシャルティコーヒーなどは、欠点・瑕疵がないことが多く『０』で進めることが多い
                     </span>
                   </li>
                   <li className={styles.edit_number_item}>
-                    5:カップの綺麗さ<span>味わいの透明度</span>
+                    5:カップの綺麗さ
+                    <br />
+                    <span className={styles.edit_number_sup}>
+                      味わいの透明度
+                    </span>
                   </li>
                   <li className={styles.edit_number_item}>
                     6:甘さ
-                    <span>味わいに甘味の印象が強ければ強い程よいとされる</span>
+                    <br />
+                    <span className={styles.edit_number_sup}>
+                      味わいに甘味の印象が強ければ強い程よいとされる
+                    </span>
                   </li>
                   <li className={styles.edit_number_item}>
                     7:酸の質
-                    <span>
+                    <br />
+                    <span className={styles.edit_number_sup}>
                       H（high）M（middle）L（low）で酸の強さを計り、得点部分には質を評価。
                     </span>
                   </li>
                   <li className={styles.edit_number_item}>
                     8:口に含んだ質感
-                    <span>
+                    <br />
+                    <span className={styles.edit_number_sup}>
                       舌触りの滑らかさ。
                       H（high）M（middle）L（low）でボディの強さを表わす。
                     </span>
                   </li>
                   <li className={styles.edit_number_item}>
-                    9:フレーバー<span>風味の質を評価する</span>
+                    9:フレーバー
+                    <br />
+                    <span className={styles.edit_number_sup}>
+                      風味の質を評価する
+                    </span>
                   </li>
                   <li className={styles.edit_number_item}>
                     10:後味の印象度
-                    <span>後味は心地よいか、そうでないか評価。</span>
+                    <br />
+                    <span className={styles.edit_number_sup}>
+                      後味は心地よいか、そうでないか評価。
+                    </span>
                   </li>
                   <li className={styles.edit_number_item}>
                     11:バランス
-                    <span>
+                    <br />
+                    <span className={styles.edit_number_sup}>
                       「５～１０」の要素に悪目立ちしているものがなく、全体のバランスが良い程加点。
                     </span>
                   </li>
                   <li className={styles.edit_number_item}>
                     12:総合評価
-                    <span>
+                    <br />
+                    <span className={styles.edit_number_sup}>
                       味わいの奥行など項目にない点にも着目し、ここまでの評価に囚われず、主観で付ける。
                     </span>
                   </li>
@@ -267,13 +291,15 @@ const New = () => {
                     13:TOTAL
                     <br />
                     （+36）
-                    <span>
+                    <br />
+                    <span className={styles.edit_number_sup}>
                       「４～１２」の得点に、定数３６点を足した１００点満点で評価。
                     </span>
                   </li>
                   <li className={styles.edit_number_item}>
                     14:味の印象
-                    <span>
+                    <br />
+                    <span className={styles.edit_number_sup}>
                       具体的な味の印象を記入。フレーバーの表現もカッピングの重要な目的。
                       <br />
                       冷めていく過程で、味わいがどのように変化したかも都度メモしておくとよい。
@@ -933,7 +959,7 @@ const New = () => {
                     </div>
                     <div className={`${styles.edit_item} ${styles.edit_total}`}>
                       <div className=""></div>
-                      <label htmlFor="total" className={styles.edit_item_title}>
+                      <label className={styles.edit_item_title}>
                         13：TOTAL（+36）
                       </label>
                       <div className={styles.edit_result}>
